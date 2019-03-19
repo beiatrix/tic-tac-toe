161bf55070b7d92ce4e44ce4d3a04716e9ebcee2
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-19 11:46:58 -0400

    Updates

---
+++ precache-manifest.70c2b3cfee91ded1dfc071262a6ac993.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "2746f85a8177d70f0f95",
+    "url": "/tic-tac-toe/static/js/main.f816a171.chunk.js"
+  },
+  {
+    "revision": "0bd08c5f063516b7ff94",
+    "url": "/tic-tac-toe/static/js/2.0c9cd3e4.chunk.js"
+  },
+  {
+    "revision": "2746f85a8177d70f0f95",
+    "url": "/tic-tac-toe/static/css/main.a1e7c937.chunk.css"
+  },
+  {
+    "revision": "1116b2bd2aaa1980989dc5019ccc5544",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
