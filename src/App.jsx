845216bd6bef67ee6a50444355ae6b565a186d6a
beiatrix <bpedrasa@gmail.com>
2024-08-24 20:25:11 -0500

    feat: init tic tac toe board

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function Square({ value }) {
+  return <button className="square">{value}</button>;
+}
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Board() {
+  const rows = Array(3).fill(null)
+  const squares = Array(3).fill(null)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    {/* rows from 1-3 */}
+    {rows.map((_, rowIndex) => (
+      <div 
+        className="board-row"
+        key={`row-${rowIndex}`}
+      >
+        {/* squares from 1-9 */}
+        {squares.map((_, squareIndex) => (
+          <Square
+            key={(rowIndex * 3) + squareIndex + 1}
+            value={(rowIndex * 3) + squareIndex + 1}
+          />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    ))}
     </>
-  )
+  );
 }
-
-export default App
